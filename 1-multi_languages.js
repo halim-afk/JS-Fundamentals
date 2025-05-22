cb88f19c926a79a5064fe94183ea8d3be39e1494
Abdelhalym Alouah <136890103+halim-afk@users.noreply.github.com>
2025-05-22 21:15:15 +0100

    Update 1-multi_languages.js

--- 1-multi_languages.js
+++ 1-multi_languages.js
@@ -1,7 +1,8 @@
-const  c = "C is fun";
-const  b = "Python is cool";
-const  d = "JavaScript is amazing";
-consol.log(c);
-consol.log(b);
-consol.log(d);
+const myVar1 = 'C is fun';
+const myVar2 = 'Python is cool';
+const myVar3 = 'JavaScript is amazing';
+
+console.log(myVar1);
+console.log(myVar2);
+console.log(myVar3);
 
