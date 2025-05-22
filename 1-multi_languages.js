4faa48defe03bc9f3e88897a8005680657f490ef
halim-afk <a.alouah0243@uca.ac.ma>
2025-05-22 21:06:16 +0100

    fixing2

--- 1-multi_languages.js
+++ 1-multi_languages.js
@@ -1,6 +1,6 @@
 const  c = "C is fun";
-const  b= "Python is cool";
-const  d= "JavaScript is amazing";
+const  b = "Python is cool";
+const  d = "JavaScript is amazing";
 consol.log(c);
 consol.log(b);
 consol.log(d);
