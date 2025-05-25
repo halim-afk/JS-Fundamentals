6ca539659a5f4ae93ce32058ddd59b8cefb34e2a
halim-afk <a.alouah0243@uca.ac.ma>
2025-05-25 22:31:54 +0100

    debuging

--- 5-to_integer.js
+++ 5-to_integer.js
@@ -1,4 +1,9 @@
-let arg1 = process.argv[2];
-if ( int(arg1) != int){
-	console.log("My number: <first argument converted in integer>");
+const arg = process.argv[2];
+const number = Number(arg);
+
+if (!isNaN(number) && Number.isInteger(number)) {
+  console.log("My number: " + number);
+} else {
+  console.log("Not a number");
 }
+
